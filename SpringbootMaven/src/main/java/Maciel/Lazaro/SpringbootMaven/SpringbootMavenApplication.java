be4c93d3a0cf7fba15d80e724bbb41f3b437d5ca
Lázaro <lazarovasconselos@gmail.com>
2020-04-19 19:34:22 -0300

    Exemplo SptingBoot Maven

---
+++ SpringbootMaven/src/main/java/Maciel/Lazaro/SpringbootMaven/SpringbootMavenApplication.java
@@ -0,0 +1,13 @@
+package Maciel.Lazaro.SpringbootMaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMavenApplication.class, args);
+	}
+
+}
---
+++ SpringbootMaven/src/test/java/Maciel/Lazaro/SpringbootMaven/SpringbootMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package Maciel.Lazaro.SpringbootMaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
