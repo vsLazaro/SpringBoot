253414efaf3434754fbf6c2f066058c9ee478f35
Lázaro <lazarovasconselos@gmail.com>
2020-04-19 19:43:08 -0300

    Exemplo com o Gradle

---
+++ ExemploGradle/src/main/java/maciel/lazaro/ExemploGradle/ExemploGradleApplication.java
@@ -0,0 +1,13 @@
+package maciel.lazaro.ExemploGradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploGradleApplication.class, args);
+	}
+
+}
---
+++ ExemploGradle/src/test/java/maciel/lazaro/ExemploGradle/ExemploGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package maciel.lazaro.ExemploGradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
